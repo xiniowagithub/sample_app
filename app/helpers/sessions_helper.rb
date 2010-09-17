a3556c9ee6000660610a5310cb6b941315509f88
xinzhang <xin-zhang@uiowa.edu>
2010-09-17 16:01:03 -0500

    Done with user edit/update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,8 +17,30 @@ module SessionsHelper
       @current_user ||= user_from_remember_token
   end
   
+  def deny_access
+    store_location
+    redirect_to signin_path, :notice => "Please sign in to access this page"
+  end
+  
   def signed_in?
       !current_user.nil?
+  end
+  
+  def current_user?(user)
+    user == current_user
+  end
+  
+  def store_location
+    session[:return_to] = request.fullpath
+  end
+  
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
+  end
+  
+  def clear_return_to
+    session[:return_to] = nil
   end
   
   private
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,26 @@ describe User do
     User.create!(@attr)
   end
   
+  describe "admin attribute" do
+    
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+    
+    it "should respond to admin" do
+      @user.should respond_to(:admin)
+    end
+    
+    it "should not be an admin by default" do
+      @user.should_not be_admin
+    end
+    
+    it "should be convertible to an admin" do
+      @user.toggle!(:admin)
+      @user.should be_admin
+    end
+  end
+  
   describe "password encryption" do
     before(:each) do
       @user = User.create!(@attr)
